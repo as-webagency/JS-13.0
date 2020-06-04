e59a9e9a18bfaac866e8fa78d06165bc8289f9fa
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-06-04 09:26:35 +0300

    add lesson 01

---
+++ lesson_01/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period;
+
+alert( 'Привет JS. Пока jQuery :)' );
+console.log( 'Привет JS. Пока jQuery :)' );
